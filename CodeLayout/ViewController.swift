77bd1330bf797c467e2bd17298aa5aedbfa93202
shuo li <shuoli84@gmail.com>
2014-11-22 22:48:35 +0800

    Initial Commit

---
+++ CodeLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeLayout
+//
+//  Created by LiShuo on 14/11/22.
+//  Copyright (c) 2014年 LiShuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
